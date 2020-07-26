3bcb9475b4e79e0b8efaba2a5087cc62ca00c4c1
starnali <46946787+starnali@users.noreply.github.com>
2020-07-26 13:28:27 -0600

    Add files via upload

---
+++ recipe.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from setuptools import setup, find_packages
+import gpt_2_simple as gpt2
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET',  'POST'])
+def generate_text():
+    
+
+    gpt2.download_gpt2()
+  
+    sess = gpt.start_tf_sess()
+    gpt2.load_gpt2(sess)
+
+    recipe = gpt2.generate(sess)
+
+    return render_template("index.html", recipe = recipe)
+	
+
+@app.route("/home")
+def test():
+    return render_template("index.html")
+if __name__ == "__main__":
+	app.run(debug=True)
